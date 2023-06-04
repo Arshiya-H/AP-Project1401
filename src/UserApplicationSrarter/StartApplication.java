0f049e8fc54b4eacf07dbaabf0abb6c7e1a00b73
mehdinap <najibpour6@gmail.com>
2023-06-04 13:48:26 +0330

    this pakage by sign up complited

--- src/UserApplicationSrarter/StartApplication.java
+++ src/UserApplicationSrarter/StartApplication.java
@@ -1,24 +1,25 @@
-package ENTER;
+package UserApplicationSrarter;
+
+import DataBase.DBManager;
 
 import java.io.IOException;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Main {
+public class StartApplication {
     public static void main(String[] args) {
+        DBManager.creatUsersTable();
         Scanner scan = new Scanner(System.in);
         try {
             Socket socket = new Socket("localhost", 2000);
             System.out.println("Choose an option : \n1 - sign up\n2 - sign in");
             String sign = scan.nextLine();
-            if (sign.equals("1")) Enter_managment.singUp();
+            if (sign.equals("1")) UserController.singUp();
             //else Enter_managment.       /////////////////      ;    //make sign in.
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 
 }
